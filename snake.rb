02afe99ff16b8c663e5885963f8feaaade44c193
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2015-11-07 08:41:19 +0000

    got snake to keep moving until another direction key is pressed

--- snake.rb
+++ snake.rb
@@ -1,7 +1,35 @@
 require 'io/console'
+require 'io/wait'
+require 'pry-byebug'
 
 def reset_grid
-  @grid = Array.new(20, ".").map{|row| Array.new(20, ".")}
+  @grid = Array.new(20, ".").map{|row| Array.new(40, ".")}
+end
+
+key_moves = {"e" => "up", "d" => "down", "o" => "left", "p" => "right", "q" => "quit", "c" => "clear" }
+
+@bugs_and_scores = {"*" => 10, "#" =>5, "?" => -5, ";" => -10, "." => 0, "@" => 0, "X" => 100} 
+
+@score = 0
+@snake = '~'
+
+def add_bug bug 
+  @grid[rand(1..18)][rand(1..38)] = bug
+end
+
+def add_bugs
+  5.times do
+    add_bug "*"
+    add_bug "#"
+    add_bug "?"
+    add_bug ";"
+  end
+  add_bug "X"
+end
+
+def set_score
+  c = @grid[@x][@y]
+  @score = @score + @bugs_and_scores[c]
 end
 
 def print_matrix
@@ -9,49 +37,116 @@ def print_matrix
     print row.join(" ")
     puts
   end
+  puts "Score: #{@score}"
+  puts "x : #{@x} y: #{@y}"
 end
 
-x = 10
-y = 10
+def game_over?
+  puts "You crashed and died - GAME OVER!"
+  return false
+end
 
-reset_grid();
-@grid[x][y]= '@'
+def reset_old_square
+  @grid[@x][@y]= '.'
+end
+
+def set_current_square
+  @grid[@x][@y]= @snake
+end
+
+def move_up
+  if @x == 0
+    game_over?
+  elsif @x > 0
+    reset_old_square
+    @x = @x - 1
+    set_score
+    set_current_square
+  end
+end
+
+def move_down
+  if @x == 19
+    game_over?
+  elsif @x < 19
+    reset_old_square
+    @x = @x + 1
+    set_score
+    set_current_square
+  end
+end
+
+def move_left
+  if @y == 0
+    game_over?
+  elsif @y > 0
+    reset_old_square
+    @y = @y - 1
+    set_score
+    set_current_square
+  end
+end
+
+def move_right
+  if @y == 39
+    game_over?
+  elsif @y < 39
+    reset_old_square
+    @y = @y + 1
+    set_score
+    set_current_square
+  end
+end
+
+def move_snake? direction
+  case direction
+    when 'up'
+      move_up
+    when 'down'
+      move_down
+    when 'left'
+      move_left
+    when 'right'
+      move_right
+    when 'clear'
+      reset_grid
+      @grid[@x][@y]= @snake
+  end
+end
+
+@x = 10
+@y = 10
+
+reset_grid
+
+@grid[@x][@y]= @snake
 
 puts `clear`
+add_bugs
 print_matrix
-c = STDIN.getch
 
-while c != 'q' do
-  case c
-    when 'e'
-      if x >= 0
-        @grid[x][y]= '.'
-        x = x - 1
-       @grid[x][y]= '@'
-      end
-    when 'd'
-      if x < 39
-        @grid[x][y]= '.'
-        x = x + 1
-        @grid[x][y]= '@'
-      end
-    when 'p'
-      if y < 39
-        @grid[x][y]= '.'
-        y = y + 1
-        @grid[x][y]= '@'
-      end
-    when 'o'
-      if y >= 0
-        @grid[x][y]= '.'
-        y = y- 1
-        @grid[x][y]= '@'
-      end
-    when  'c'
-      reset_grid
-      @grid[x][y]= '@'
+term = `stty -g`
+`stty raw -echo cbreak`
+@direction = ''
+loop do
+  `clear`
+  if STDIN.ready?
+    command = STDIN.getc
+    if command == 'q' 
+      break
+    end
+    @direction = command
   end
+  if key_moves.has_key?(command)
+    @direction = command
+  end
+
+  if move_snake?(key_moves[@direction]) == false
+    break
+  end
+
+  sleep 0.2
   puts `clear`
   print_matrix
-  c = STDIN.getch
 end
+
