6ee8ef816c4b2f5244361da67eddf7174c2a2f60
Sandy McMillan <sandy.mcmillan@codeclan.com>
2016-06-23 15:48:39 +0100

    got it working moving horizontally

--- cave/column.rb
+++ cave/column.rb
@@ -4,9 +4,9 @@ class Column
   attr_accessor :cave_top, :cave_bottom, :column, :column_height
 
   def initialize (prev_cave_top, cave_height)
-    @column_height = 40
-    @cave_top = prev_cave_top + rand(-1..2)
-    @cave_bottom = @column_height - rand(10..20)
+    @column_height = 20
+    @cave_top = prev_cave_top + rand(-1..1)
+    @cave_bottom = @column_height - rand(5..10)
     @column = []
   end
 
@@ -14,9 +14,11 @@ class Column
     #puts @cave_top
     for i in 0..@column_height-1
       @column[i] = " ".bg_green
+      #@column[i] = "."
+
     end
     for j in @cave_top..@cave_bottom
-      @column[j] = " ".bg_blue
+      @column[j] = " "#.bg_blue
     end
   end
 
